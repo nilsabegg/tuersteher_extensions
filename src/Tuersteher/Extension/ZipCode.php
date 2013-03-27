84325632687521adf5e7b5649cf1ee00d12c4d33
Nils Abegg <nils.abegg@ffuf.de>
2013-03-27 03:29:29 +0100

    no message

--- src/Tuersteher/Extension/ZipCode.php
+++ src/Tuersteher/Extension/ZipCode.php
@@ -1,18 +1,57 @@
 <?php
 
+/**
+ * This file is part of the Türsteher library.
+ */
 namespace Tuersteher\Extension;
 
 use \Guzzle\Http\Client as HttpClient;
 use \Tuersteher\Exception\InvalidArgument as InvalidArgumentException;
 use \Tuersteher\Validator\Validator as Validator;
 
+/**
+ * ZipCode
+ *
+ * This class is a zip code validator for the Türsteher library.
+ * The zip code can be validated by various webservices.
+ *
+ * @author      Nils Abegg <tuersteher@nilsabegg.de>
+ * @version     0.2
+ * @package     Türsteher Extensions
+ * @category    Validation
+ */
 class ZipCode extends Validator
 {
 
+    /**
+     * country
+     *
+     * Holds the country code for the zip code.
+     * The country code has to be in ISO-3166 format.
+     *
+     * @access protected
+     * @var    string
+     */
     protected $country = 'DE';
 
+    /**
+     * country
+     *
+     * Holds the webservice to validate the zip code.
+     *
+     * @access protected
+     * @var    string
+     */
     protected $service = 'geonames';
 
+    /**
+     * country
+     *
+     * Holds the username for the webservice.
+     *
+     * @access protected
+     * @var    string
+     */
     protected $username = 'demo';
 
     public function country($country)
@@ -55,7 +94,7 @@ class ZipCode extends Validator
 
             return $this->createResult(true, 'bla');
         } else {
-            
+
             return $this->createResult(false, 'bla');
         }
 
@@ -88,7 +127,7 @@ class ZipCode extends Validator
         $client = new HttpClient('http://zip.elevenbasetwo.com');
         $request = $client->get('/v2/' . urlencode($this->country) . '/' . urlencode($zip));
         try {
-            $response = $request->send();
+            $request->send();
 
             return true;
         } catch (\Guzzle\Http\Exception\ClientErrorResponseException $exception) {
